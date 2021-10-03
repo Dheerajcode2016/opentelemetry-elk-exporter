2baa17b2db76ed21509e6da3812ed8920b38d6d6
dheerajcode2016 <dheerajcode2016@outlook.com>
2021-10-03 14:27:31 +0530

    Progress 1. Added extention classes

---
+++ OpenTelemetry.Exporter.ElasticCache/OpenTelemetry.Exporter.ElasticCache/ElasticCacheExporterHelperExtensions.cs
@@ -0,0 +1,34 @@
+﻿using OpenTelemetry.Exporter;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace OpenTelemetry.Trace
+{
+  public static class ElasticCacheExporterHelperExtensions
+  {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "The objects should not be disposed.")]
+    public static TracerProviderBuilder AddInMemoryExporter(this TracerProviderBuilder builder, ICollection<Activity> exportedItems)
+    {
+      if (builder == null)
+      {
+        throw new ArgumentNullException(nameof(builder));
+      }
+
+      if (exportedItems == null)
+      {
+        throw new ArgumentNullException(nameof(exportedItems));
+      }
+
+      if (builder is IDeferredTracerProviderBuilder deferredTracerProviderBuilder)
+      {
+        return deferredTracerProviderBuilder.Configure((sp, builder) =>
+        {
+          builder.AddProcessor(new SimpleActivityExportProcessor(new ElasticCacheExporter<Activity>(exportedItems)));
+        });
+      }
+
+      return builder.AddProcessor(new SimpleActivityExportProcessor(new ElasticCacheExporter<Activity>(exportedItems)));
+    }
+  }
+}
---
+++ OpenTelemetry.Exporter.ElasticCache/OpenTelemetry.Exporter.ElasticCache/ElasticCacheExporterLoggingExtentions.cs
@@ -0,0 +1,24 @@
+﻿using OpenTelemetry.Exporter;
+using System;
+using System.Collections.Generic;
+
+namespace OpenTelemetry.Logs
+{
+  public static class ElasticCacheExporterLoggingExtentions
+  {
+    public static OpenTelemetryLoggerOptions AddElasticCacheExporter(this OpenTelemetryLoggerOptions loggerOptions, ICollection<LogRecord> exportedItems)
+    {
+      if (loggerOptions == null)
+      {
+        throw new ArgumentNullException(nameof(loggerOptions));
+      }
+
+      if (exportedItems == null)
+      {
+        throw new ArgumentNullException(nameof(exportedItems));
+      }
+
+      return loggerOptions.AddProcessor(new SimpleLogRecordExportProcessor(new ElasticCacheExporter<LogRecord>(exportedItems)));
+    }
+  }
+}
