47de9ea1493a35c05775a56fcd7d1735c7aad650
dheerajcode2016 <dheerajcode2016@outlook.com>
2021-12-12 13:48:46 +0530

    Added retry logic and updated gitignore file

--- OpenTelemetry.Exporter.ElasticCache/OpenTelemetry.Exporter.ElasticCache/Options/ElasticsearchExporterOptions.cs
+++ OpenTelemetry.Exporter.ElasticCache/OpenTelemetry.Exporter.ElasticCache/Options/ElasticsearchExporterOptions.cs
@@ -7,10 +7,22 @@ namespace OTel.Exporter.Elasticsearch.Options
     public Uri Host { get; set; }
     public string DefaultIndex { get; set; }
 
+    public TimeSpan WaitBeforeNextRetry { get; set; } = TimeSpan.FromSeconds(1);
+
+    public int TotalRetry { get; set; } = 3;
+
     public ElasticsearchExporterHttpOptions(string DefaultIndex, string HostUrl)
     {
       this.Host = new Uri(HostUrl);
       this.DefaultIndex = DefaultIndex;
+    }
+
+    public ElasticsearchExporterHttpOptions(string DefaultIndex, string HostUrl, TimeSpan waitBeforeNextRetry, int totalRetry)
+    {
+      this.Host = new Uri(HostUrl);
+      this.DefaultIndex = DefaultIndex;
+      this.WaitBeforeNextRetry = waitBeforeNextRetry;
+      this.TotalRetry = totalRetry;
     }
   }
 }
