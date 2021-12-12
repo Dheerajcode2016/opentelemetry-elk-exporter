42842cb03de3d70f5560ee9b63547f7a907914fd
Dheeraj Kumar <25458817+Dheerajcode2016@users.noreply.github.com>
2021-12-12 14:46:24 +0530

    Develop (#4)
    * Started working on Adding retry logic
    
    * Added retry logic and updated gitignore file
    
    * Update HttpConnector.cs
    
    * Update codeql-analysis.yml
    
    * Update codeql-analysis.yml
    
    * Setting Release version 2.0.0
    
    * Updated release version

--- OpenTelemetry.Exporter.ElasticCache/OpenTelemetry.Exporter.ElasticCache/Connector/HttpConnector.cs
+++ OpenTelemetry.Exporter.ElasticCache/OpenTelemetry.Exporter.ElasticCache/Connector/HttpConnector.cs
@@ -4,6 +4,8 @@ using OTel.Exporter.Elasticsearch.Models;
 using OpenTelemetry.Logs;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
 
 namespace OTel.Exporter.Elasticsearch.Connector
 {
@@ -12,53 +14,67 @@ namespace OTel.Exporter.Elasticsearch.Connector
     protected ConnectionSettings Settings { get; set; }
     protected ElasticClient Client { get; set; }
 
+    protected TimeSpan WaitBeforeRetry { get; set; }
+
+    protected int TotalRetry { get; set; }
+
     internal HttpConnector(ElasticsearchExporterHttpOptions options) : base()
+
     {
       this.Settings = new ConnectionSettings(options.Host).DefaultIndex(options.DefaultIndex);
+      this.WaitBeforeRetry = options.WaitBeforeNextRetry;
+      this.TotalRetry = options.TotalRetry;
     }
-    
-    #region Log_Export
-        internal override void Push(LogRecord objectToPush)
-        {
-            Task.Run(() =>
-            {
-                var s = GetClient().IndexDocumentAsync(Transform(objectToPush)).Result;
-            });
-        }
 
-        private LogMessage Transform(LogRecord objectToPush)
+    #region Log_Export
+
+    internal override void Push(LogRecord objectToPush)
+    {
+      Task.Run(() =>
+      {
+        Retry.Do(() =>
         {
-            LogMessage message = new LogMessage();
-            if (objectToPush != null)
-            {
-                message.TraceId = objectToPush.TraceId;
-                message.SpanId = objectToPush.SpanId;
-                message.LogLevel = objectToPush.LogLevel;
-                message.TraceFlags = objectToPush.TraceFlags;
-                message.TraceState = objectToPush.TraceState;
-                message.Timestamp = objectToPush.Timestamp;
-                message.Exception = objectToPush.Exception;
-                message.CategoryName = objectToPush.CategoryName;
-                message.EventId = objectToPush.EventId;
-                if (objectToPush.FormattedMessage != null)
-                {
-                    message.Message = objectToPush.FormattedMessage;
-                }
-                if (objectToPush.State != null)
-                {
-                    message.Message = objectToPush.State.ToString();
-                }
-            }
-            return message;
+          var s = GetClient().IndexDocumentAsync(Transform(objectToPush)).Result;
+        }, WaitBeforeRetry, TotalRetry);
+      }).ConfigureAwait(false);
+    }
+
+    private LogMessage Transform(LogRecord objectToPush)
+    {
+      LogMessage message = new LogMessage();
+      if (objectToPush != null)
+      {
+        message.TraceId = objectToPush.TraceId;
+        message.SpanId = objectToPush.SpanId;
+        message.LogLevel = objectToPush.LogLevel;
+        message.TraceFlags = objectToPush.TraceFlags;
+        message.TraceState = objectToPush.TraceState;
+        message.Timestamp = objectToPush.Timestamp;
+        message.Exception = objectToPush.Exception;
+        message.CategoryName = objectToPush.CategoryName;
+        message.EventId = objectToPush.EventId;
+        if (objectToPush.FormattedMessage != null)
+        {
+          message.Message = objectToPush.FormattedMessage;
         }
-        #endregion
+        if (objectToPush.State != null)
+        {
+          message.Message = objectToPush.State.ToString();
+        }
+      }
+      return message;
+    }
+
+    #endregion Log_Export
 
     #region Trace_Export
-        internal override void Push(Activity objectToPush)
-        {
-            GetClient().IndexDocumentAsync(objectToPush);
-        }
-        #endregion
+
+    internal override void Push(Activity objectToPush)
+    {
+      Retry.Do(() => { var t = GetClient().IndexDocumentAsync(objectToPush).Result; }, WaitBeforeRetry, TotalRetry);
+    }
+
+    #endregion Trace_Export
 
     private ElasticClient GetClient()
     {
